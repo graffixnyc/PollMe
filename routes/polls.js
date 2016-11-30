2d5219efc7ea8102b90f7fec481eb42d90de84c6
Jason Sarwar <jsarwar@stevens.edu>
2016-11-30 17:23:56 -0500

    A few route updates

--- routes/polls.js
+++ routes/polls.js
@@ -8,13 +8,17 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 
-router.get("/makepoll", function (request, response) {
+router.get("/addpoll", function (request, response) {
     if(req.isAuthenticated()) {
         //Render the make poll page or something like that
         //request.user.username has username of user
     }
     else {
         //Render a login page
+        if(request.flash().error)
+            response.render('pollme/loginpage', { error: request.flash().error, redirectPage: "/makepoll" });
+        else
+            response.render('pollme/loginpage', { redirectPage: "/makepoll" });
     }
 });
     
@@ -23,5 +27,38 @@ router.get("/poll/:id", function (request, response) {
     
 });
 
+router.post("/voteonpoll", function (request, response) {
+    
+    //I guess the poll id should in the request somewhere
+    
+    if(req.isAuthenticated()) {
+        // Allowed to vote on poll
+        // request.user.username has username of user
+    }
+    else {
+        //Render a login page
+        if(request.flash().error)
+            response.render('pollme/loginpage', { error: request.flash().error, redirectPage: "/" });
+        else
+            response.render('pollme/loginpage', { redirectPage: "/" });
+    }
+});
+
+router.post("/commentonpoll", function (request, response) {
+    
+    //I guess the poll id should in the request somewhere
+    
+    if(req.isAuthenticated()) {
+        // Allowed to comment on poll
+        // request.user.username has username of user
+    }
+    else {
+        //Render a login page
+        if(request.flash().error)
+            response.render('pollme/loginpage', { error: request.flash().error, redirectPage: "/" });
+        else
+            response.render('pollme/loginpage', { redirectPage: "/" });
+    }
+});
 
 module.exports = router;
