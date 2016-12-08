0be9dfe9bd617aa3e4a1a1d56f08653eb3e78625
Patrick Hill <graffixnyc@gmail.com>
2016-12-08 13:10:42 -0500

    added constructorMethod

--- data/index.js
+++ data/index.js
@@ -2,8 +2,11 @@ const users = require("./users");
 const polls = require("./polls")
 const votesAndMetrics = require("./votesandmetrics")
 
-module.exports = {
-    users: users,
-    polls: polls,
-    votesAndMetrics: votesAndMetrics
+const constructorMethod = (app) => {
+    app.use("/users", users);
+    app.use("/polls", polls);
+    app.use("*", (req, res) => {
+        res.status(404).json({ error: "Not found" });
+    });
 };
+module.exports = constructorMethod;
