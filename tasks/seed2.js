4abfd0627479c9d9201ab51aea4c51846b82a875
hainiaoyu <hli47@stevens.edu>
2016-12-09 10:16:43 -0500

    Test seed2 to database

--- tasks/seed2.js
+++ tasks/seed2.js
@@ -4,15 +4,15 @@ const users = data.users;
 const polls = data.polls;
 const votesAndMetrics =data.votesAndMetrics;
 const bcrypt = require('bcryptjs');
-  return votesAndMetrics.countVote("74bd807d-8294-4325-b195-fa657dad41bb", 0,0,0,1 ,"b7f8fc28-9b41-4c4c-ab63-8a62214c58a5","M").then(() => {
-    return votesAndMetrics.countVote("e4281c72-1a83-4a60-b8db-a5fa45efe9dc", 1,0,0,0 ,"b7f8fc28-9b41-4c4c-ab63-8a62214c58a5","M").then(() => {
+  return votesAndMetrics.countVote("425105e3-be7a-4bea-9458-d6785f865d85", 0,0,0,1 ,"ea3af022-c994-4d2e-9a7b-36965cdc8396","M").then(() => {
+    return votesAndMetrics.countVote("26fd4c56-e1e4-4c1a-a388-75dc69d5f1f4", 1,0,0,0 ,"ea3af022-c994-4d2e-9a7b-36965cdc8396","M").then(() => {
         
      
     
-// }, (error) => {
-//     console.error(error);
-// });
-// });
+}, (error) => {
+    console.error(error);
+});
+});
 // let poll1={}
 //   return polls.updatePoll("74bd807d-8294-4325-b195-fa657dad41bb",poll1 )
 
