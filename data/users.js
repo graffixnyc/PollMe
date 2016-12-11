50bbad16f0c8db7958c1d981843d9c937ff3a1b8
Patrick Hill <graffixnyc@gmail.com>
2016-12-10 22:02:59 -0500

    working on updateUser

--- data/users.js
+++ data/users.js
@@ -91,6 +91,8 @@ let exportedMethods = {
             if (currentUser.gender != updatedUser) {
                 //get votes for polls they voted in deincrement the total votes for the gener they were
                 //and then increment the total number of votes for their new gender.  
+                var pollsVotedIn=currentUser.pollsVotedIn;
+                console.log(pollsVotedIn.length);
                 // return polls.getPollsByUser(id).then((polls) => {
                 //     for (let i = 0; i < polls.length; i++) {
                 //         console.log ("POLL ID: " + polls[i]._id )
@@ -100,25 +102,25 @@ let exportedMethods = {
                 //     }
                 // })
             }
-            let updatedUser = {
-                firstName: updatedUser.firstName,
-                lastName: updatedUser.lastName,
-                email: upatedUser.email,
-                gender: upatedUser.gender,
-                city: updateUser.city,
-                state: upatedUser.state,
-                age: upatedUser.age,
-                hashedPassword: upatedUser.hashedPassword
-            };
+        //     let updatedUser = {
+        //         firstName: updatedUser.firstName,
+        //         lastName: updatedUser.lastName,
+        //         email: upatedUser.email,
+        //         gender: upatedUser.gender,
+        //         city: updateUser.city,
+        //         state: upatedUser.state,
+        //         age: upatedUser.age,
+        //         hashedPassword: upatedUser.hashedPassword
+        //     };
 
-            let updateCommand = {
-                $set: updatedUser
-            };
+        //     let updateCommand = {
+        //         $set: updatedUser
+        //     };
 
-            return userCollection.updateOne({ _id: id }, updateCommand).then(() => {
-                return this.getUserById(id);
-            });
-        });
+        //     return userCollection.updateOne({ _id: id }, updateCommand).then(() => {
+        //         return this.getUserById(id);
+        //     });
+         });
     },
     addPollCreatedToUser(userId, pollId) {
         return users().then((userCollection) => {
@@ -157,6 +159,17 @@ let exportedMethods = {
                 }
             });
         });
+    },
+    getPollsUserVotedin(userId){
+         if (!userId)
+            return Promise.reject("No userId provided");
+
+        return this.getUserById(userId).then((user) => {
+            var pollsVotedIn = user.pollsVotedIn;
+            console.log(pollsVotedIn.length)
+                
+        });
+
     },
 
     isPasswordValid(user, password) {
