4ddd8bac7b550e796dd04546d03fbfa66daf3685
Patrick Hill <graffixnyc@gmail.com>
2016-12-12 08:44:09 -0500

    fixed mypolls route

--- routes/users.js
+++ routes/users.js
@@ -93,27 +93,24 @@ router.get('/private', isLoggedIn, function (req, res) {
 });
 
 router.get('/mypolls', isLoggedIn, function (req, res) {
-    let userResult = {};
-    userResult.userInfo = req.user;
-    // console.log(userResult.userInfo);
-    let pollsInfo = [];
-    for (i = 0; i < userResult.userInfo.pollsCreated.length; i++) {
-        let subpoll = {};
-        pollsData.getPollById(userResult.userInfo.pollsCreated[i].pollId).then((poll) => {
-            subpoll._id = poll._id;
-            subpoll.question = poll.question;
-            subpoll.category = poll.category;
-            subpoll.postedDate = poll.postedDate;
-            votesmatrixData.getVotesForPoll(poll._id).then((votes) => {
-                subpoll.vote = votes.totalVotesForPoll;
+    console.log(req.user._id)
+     pollsData.getPollsByUser(req.user._id).then((polls) => {
+        let pollsInfo = [];
+        for (i = 0; i < polls.length; i++) {
+            let subpoll = {};
+            subpoll._id = polls[i]._id;
+            subpoll.question = polls[i].question;
+            subpoll.category = polls[i].category;
+            subpoll.postedDate = polls[i].postedDate;
+            votesmatrixData.getVotesForPoll(polls[i]._id).then((votes) => {
+                if (votes) {
+                    subpoll.votes = votes.totalVotesForPoll;
+                }
             })
             pollsInfo.push(subpoll);
-        })
-    };
-    userResult.pollInfo = pollsInfo;
-    res.render('pollme/mypage_mypoll', {
-        user: userResult
-    });
+        }
+        res.render("pollme/mypage_mypoll", { poll: pollsInfo });
+    })
 
 });
 
